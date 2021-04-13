0d8e477ad72c96838650046c6ec2a4bd60b809e7
juan rios <juan.rios49@eia.edu.co>
2021-04-13 11:09:22 -0500

    Move Components

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,13 +3,13 @@ import '../assets/css/App.css';
 import 'antd/dist/antd.css';
 
 import AppHeader from "../components/header";
-import AppHero from "../components/hero";
-import AppAbout from "../components/about";
-import AppFeature from "../components/aFeature";
-import AppWorks from "../components/works";
-import AppPricing from "../components/princing";
-import AppContact from "../components/contact";
-import AppFooter from "../components/footer";
+import AppHero from "../components/main/hero";
+import AppAbout from "../components/main/about";
+import AppFeature from "../components/main/aFeature";
+import AppWorks from "../components/main/works";
+import AppPricing from "../components/main/princing";
+import AppContact from "../components/main/contact";
+import AppFooter from "../components/main/footer";
 
 import { Layout } from 'antd';
 import {Footer} from "antd/es/layout/layout";
